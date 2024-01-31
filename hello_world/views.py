492a7dc2c55b51b3161f67d9f2b617c226be82af
roc-11 <>
2024-01-31 21:45:07 +0000

    Fix indentation erorr

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     if request.method == "GET":
-       return HttpResponse("This was a GET request")
-   elif request.method == "POST":
-       return HttpResponse("This was a POST request")
+        return HttpResponse("This was a GET request")
+    elif request.method == "POST":
+        return HttpResponse("This was a POST request")
